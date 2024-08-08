689cee0ff4a5b36367c6c2a47c6179a82aa7a83b
Amreth73 <117499632+Amreth73@users.noreply.github.com>
2024-08-08 15:17:31 +0530

    JWT implemented

---
+++ backend/partymanagement/src/main/java/com/example/partymanagement/Controller/VenueController.java
@@ -0,0 +1,29 @@
+package com.example.partymanagement.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.partymanagement.Model.VenueModel;
+import com.example.partymanagement.Service.VenueService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/venues")
+@CrossOrigin("http://localhost:3000")
+public class VenueController {
+
+    @Autowired
+    private VenueService service;
+
+    @PostMapping("/add")
+    public String postMethodName(@RequestBody VenueModel obj) {
+        service.postData(obj);
+        return "Done";
+    }
+    
+}
---
+++ backend/partymanagement/src/main/java/com/example/partymanagement/Repository/paymentRepository.java
@@ -0,0 +1,10 @@
+package com.example.partymanagement.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.partymanagement.Model.PaymentModel;
+
+public interface paymentRepository extends JpaRepository<PaymentModel,Long> {
+
+    
+} 
